1c0bb1ec413969ed3d4020b52f2f926f78cd05ce
iannik <iannik@users.noreply.github.com>
2026-07-27 18:41:43 +0000

    deploy: 104af5f590f4e8aaf33a0882abab9193257d5d56

--- TestAutomationShowcase.Core/HttpRequestOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TestAutomationShowcase.Core;
-
-public static class HttpRequestOptions
-{
-    public static readonly HttpRequestOptionsKey<bool> SkipAuthentication = new("SkipAuthentication");
-}
--- TestAutomationShowcase.Core/Models/SauceDemo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestAutomationShowcase.Core.Models;
-
-public class ShippingDetails
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PostalCode { get; set; }
-}
